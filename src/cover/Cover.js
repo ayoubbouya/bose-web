cf36db3f7cfec70cc424f6ca2a20e1de1f97f2d1
ayoubbouya <bouya.ayoub1@gmail.com>
2021-02-05 23:47:57 +0200

    updated cover

--- src/cover/Cover.js
+++ src/cover/Cover.js
@@ -6,8 +6,7 @@ import union from "../img/Union.png";
 import union1 from "../img/Union-1.png";
 
 function Cover() {
-  const [selected, setSelected] = useState("Silver");
-  const [selectedStyle, setSelectedStyle] = useState(0);
+  const [selected, setSelected] = useState("color1");
 
   return (
     <div>
@@ -22,63 +21,143 @@ function Cover() {
               className={
                 styles.ellipse1 +
                 " " +
-                (selectedStyle === 0 ? styles.selectedEllipse : "")
+                (selected === "color1" ? styles.selectedDefault1 : "")
               }
               onClick={() => {
-                setSelectedStyle(0);
-              }}
-              onClick={() => {
-                setSelected("Silver");
+                setSelected("color1");
               }}
             >
-              <div className={styles.circle1}>
-                <div className={styles.circle2}>
-                  <div className={styles.circle3}></div>
+              <div className={selected === "color1" ? styles.circle1 : ""}>
+                <div className={selected === "color1" ? styles.circle2 : ""}>
+                  <div
+                    className={selected === "color1" ? styles.circle3 : ""}
+                  ></div>
+                </div>
+              </div>
+            </li>
+
+            <li
+              className={
+                styles.ellipse2 +
+                " " +
+                (selected === "color2" ? styles.selectedDefault2 : "")
+              }
+              onClick={() => {
+                setSelected("color2");
+              }}
+            >
+              <div className={selected === "color2" ? styles.circle01 : ""}>
+                <div className={selected === "color2" ? styles.circle02 : ""}>
+                  <div
+                    className={selected === "color2" ? styles.circle03 : ""}
+                  ></div>
                 </div>
               </div>
             </li>
             <li
               className={
-                styles.ellipse2 +
-                " " +
-                (selected === "Ellipse 2" ? styles.selectedEllipse : "")
-              }
-              onClick={() => {
-                setSelected("Ellipse 2");
-              }}
-            ></li>
-            <li
-              className={
                 styles.ellipse3 +
                 " " +
-                (selected === "Ellipse 3" ? styles.selectedEllipse : "")
+                (selected === "color3" ? styles.selectedDefault3 : "")
               }
               onClick={() => {
-                setSelected("Ellipse 3");
+                setSelected("color3");
               }}
-            ></li>
+            >
+              <div className={selected === "color3" ? styles.circle001 : ""}>
+                <div className={selected === "color3" ? styles.circle002 : ""}>
+                  <div
+                    className={selected === "color3" ? styles.circle003 : ""}
+                  ></div>
+                </div>
+              </div>
+            </li>
+
             <li
               className={
                 styles.ellipse4 +
                 " " +
-                (selected === "Ellipse 2" ? styles.selectedEllipse : "")
+                (selected === "color4" ? styles.selectedDefault4 : "")
               }
               onClick={() => {
-                setSelected("Ellipse 4");
+                setSelected("color4");
               }}
-            ></li>
+            >
+              <div className={selected === "color4" ? styles.circle0001 : ""}>
+                <div className={selected === "color4" ? styles.circle0002 : ""}>
+                  <div
+                    className={selected === "color4" ? styles.circle0003 : ""}
+                  ></div>
+                </div>
+              </div>
+            </li>
             <li
               className={
                 styles.ellipse5 +
                 " " +
-                (selected === "Ellipse 5" ? styles.selectedEllipse : "")
+                (selected === "color5" ? styles.selectedDefault5 : "")
               }
               onClick={() => {
-                setSelected("Ellipse 5");
+                setSelected("color5");
               }}
-            ></li>
+            >
+              {" "}
+              <div className={selected === "color5" ? styles.circle00001 : ""}>
+                <div
+                  className={selected === "color5" ? styles.circle00002 : ""}
+                >
+                  <div
+                    className={selected === "color5" ? styles.circle00003 : ""}
+                  ></div>
+                </div>
+              </div>
+            </li>
           </ul>
-          <span className={styles.selectedDefault}>{selected}</span>
+          <span
+            className={
+              styles.selected +
+              " " +
+              (selected === "color1" ? styles.color1 : styles.hide)
+            }
+          >
+            {selected}
+          </span>
+          <span
+            className={
+              styles.selected +
+              " " +
+              (selected === "color2" ? styles.color2 : styles.hide)
+            }
+          >
+            {selected}
+          </span>
+          <span
+            className={
+              styles.selected +
+              " " +
+              (selected === "color3" ? styles.color3 : styles.hide)
+            }
+          >
+            {selected}
+          </span>
+          <span
+            className={
+              styles.selected +
+              " " +
+              (selected === "color4" ? styles.color4 : styles.hide)
+            }
+          >
+            {selected}
+          </span>
+          <span
+            className={
+              styles.selected +
+              " " +
+              (selected === "color5" ? styles.color5 : styles.hide)
+            }
+          >
+            {selected}
+          </span>
 
           <div className={styles.priceContainer}>
             <span className={styles.price}>229.95$</span>
